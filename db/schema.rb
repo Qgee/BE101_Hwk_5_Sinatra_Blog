4e7af8903ca031eff63f450c56985091d17b1e5f
Aaron Yap <aynycda@gmail.com>
2015-06-06 11:39:25 -0400

    added blog table, html pages to show users, user detail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604024145) do
+ActiveRecord::Schema.define(version: 20150606140736) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
--- sinatra_blog.rb
+++ sinatra_blog.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './user'
+require './post'
 
 set :database, "sqlite3:blogapp.sqlite3"
+
+
+get '/users' do
+	@users = User.all
+	erb :users
+end
+
+get '/user/:id' do
+	@user = User.find_by_id(params[:id])
+	erb :user_detail
+end
